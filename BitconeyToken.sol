1c84d5c064c0cde39981ed72225997cd8f9817b7
Gregory Rogachev <kotsmilewtm@gmail.com>
2023-01-11 14:32:23 +0300

    add token contract

---
+++ BitconeyToken.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title BitconeyToken
+ * @author gotbit
+ */
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract BitconeyToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 21_000_000e8);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 8;
+    }
+}
+
